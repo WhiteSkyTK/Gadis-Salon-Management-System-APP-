5dd1340f821e011d7b0935a0da99ef453daec146
Tokollo Will Nonyane <128146791+WhiteSkyTK@users.noreply.github.com>
2025-07-31 11:49:40 +0200

    fragments added

---
+++ app/src/main/java/com/rst/gadissalonmanagementsystemapp/ui/home/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.rst.gadissalonmanagementsystemapp.ui.home
+
+import androidx.fragment.app.Fragment
+import com.rst.gadissalonmanagementsystemapp.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+}
