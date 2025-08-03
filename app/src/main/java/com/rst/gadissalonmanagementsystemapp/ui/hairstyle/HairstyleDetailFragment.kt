10813712757056bbed11b28b32501b825cb0d5b0
Tokollo Will Nonyane <128146791+WhiteSkyTK@users.noreply.github.com>
2025-08-03 12:28:03 +0200

    Added the Profile Fragments

---
+++ app/src/main/java/com/rst/gadissalonmanagementsystemapp/ui/hairstyle/HairstyleDetailFragment.kt
@@ -0,0 +1,7 @@
+package com.rst.gadissalonmanagementsystemapp.ui.hairstyle
+
+import androidx.fragment.app.Fragment
+import com.rst.gadissalonmanagementsystemapp.R
+
+class HairstyleDetailFragment : Fragment(R.layout.fragment_hairstyle_detail) {
+}
